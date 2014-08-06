b3f50f2b098250dca41d8a25c8aee6a39f79a6af
Mandi Grant <mandi018@gmail.com>
2014-08-06 11:49:15 -0700

    Added a conditional to validate whether the current date is actually prior to the end date. If current date exceeds end date, print 0.

--- static/js/script.js
+++ static/js/script.js
@@ -7,7 +7,13 @@ function calcDaysRemaining() {
   var today, endDate, daysRemaining;
   today = new Date();
   endDate = new Date(2014, 8, 25, 23, 59, 59);
-  daysRemaining = Math.floor((endDate - today) / 86400000);
+  if (today < endDate) {
+    daysRemaining = Math.floor((endDate - today) / 86400000);
+  }
+  else {
+    daysRemaining = 0;
+  }
+  
 
   $(".today").html("Today is " + prettyDate(today) + "<br> Class ends on " + prettyDate(endDate));
   $(".daysLeft").html(daysRemaining);
